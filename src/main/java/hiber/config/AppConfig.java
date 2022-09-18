d2f0bf9cd9d7212c0c412f0e26a81526b1ff10bb
Sergey Ryabinin <ryabinin.sergey@gmail.com>
2022-09-19 01:58:59 +0300

    first start

--- src/main/java/hiber/config/AppConfig.java
+++ src/main/java/hiber/config/AppConfig.java
@@ -15,44 +15,53 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import javax.sql.DataSource;
 import java.util.Properties;
 
-
 @Configuration
 @PropertySource("classpath:db.properties")
 @EnableTransactionManagement
 @ComponentScan(value = "hiber")
 public class AppConfig {
 
-   @Autowired
-   private Environment env;
+  @Autowired
+  private Environment env;
 
-   @Bean
-   public DataSource getDataSource() {
-      DriverManagerDataSource dataSource = new DriverManagerDataSource();
-      dataSource.setDriverClassName(env.getProperty("db.driver"));
-      dataSource.setUrl(env.getProperty("db.url"));
-      dataSource.setUsername(env.getProperty("db.username"));
-      dataSource.setPassword(env.getProperty("db.password"));
-      return dataSource;
-   }
+  @Bean
+  public DataSource getDataSource() {
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
-   @Bean
-   public LocalSessionFactoryBean getSessionFactory() {
-      LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
-      factoryBean.setDataSource(getDataSource());
-      
-      Properties props=new Properties();
-      props.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
-      props.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+    dataSource.setDriverClassName(env.getProperty("com.mysql.cj.jdbc.Driver"));
+    dataSource.setUrl(env.getProperty(
+        "jdbc:mysql://localhost:3306/pp221?verifyServerCertificate=false&useSSL=false&"
+            + "requireSSL=false&useLegacyDatetimeCode=false&amp&serverTimezone=UTC"));
+    dataSource.setUsername(env.getProperty("pp221"));
+    dataSource.setPassword(env.getProperty("Pp221Apple1976@@@###Fuck"));
+    /*
+     * dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+     * dataSource.setUrl("jdbc:mysql://localhost:3306/pp221");
+     * dataSource.setUsername("pp221");
+     * dataSource.setPassword("Pp221Apple1976@@@###Fuck");
+     */
+    return dataSource;
 
-      factoryBean.setHibernateProperties(props);
-      factoryBean.setAnnotatedClasses(User.class);
-      return factoryBean;
-   }
+  }
 
-   @Bean
-   public HibernateTransactionManager getTransactionManager() {
-      HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-      transactionManager.setSessionFactory(getSessionFactory().getObject());
-      return transactionManager;
-   }
+  @Bean
+  public LocalSessionFactoryBean getSessionFactory() {
+    LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+    factoryBean.setDataSource(getDataSource());
+
+    Properties props = new Properties();
+    props.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+    props.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+
+    factoryBean.setHibernateProperties(props);
+    factoryBean.setAnnotatedClasses(User.class);
+    return factoryBean;
+  }
+
+  @Bean
+  public HibernateTransactionManager getTransactionManager() {
+    HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+    transactionManager.setSessionFactory(getSessionFactory().getObject());
+    return transactionManager;
+  }
 }
