770a27b31f579e166d04cfce5a0eec4ea2f8a9d7
Sergey Ryabinin <ryabinin.sergey@gmail.com>
2022-09-19 12:29:33 +0300

    search models using path to models

---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,51 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "car")
+public class Car {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "model")
+  private String model;
+
+  @Column(name = "series")
+  private Integer series;
+
+  public Car() {
+  }
+
+  public Car(String model, Integer series) {
+    this.model = model;
+    this.series = series;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getModel() {
+    return model;
+  }
+
+  public void setModel(String model) {
+    this.model = model;
+  }
+
+  public Integer getSeries() {
+    return series;
+  }
+
+  public void setSeries(Integer series) {
+    this.series = series;
+  }
+
+}
